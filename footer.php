fa3f836448a5e4d8cae23bcf8ffc8d7b6e6dd1a3
Jazmin Dominguez <jdominguez13@ucol.mx>
2024-09-14 00:35:47 -0600

    en este caos el footer lo pase a codigo php para reutilizar codigo

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Footer Example</title>
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            font-family: Arial, sans-serif;
+        }
+
+        .content {
+            /*min-height: 100%;
+            padding-bottom: 60px; /* Debe ser igual a la altura del footer */
+            flex: 1;
+            padding-bottom:60px;
+            box-sizing: border-box;
+        }
+
+        .footer {
+            bottom: 0;
+            width: 100%;
+            height: 80px; /* Altura del footer */
+            background-color: #355962; /* Color de fondo */
+            text-align: center;
+            padding: 10px 0;
+            box-sizing: border-box;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .footer img {
+            width: 40px;
+            height: auto;
+            vertical-align: middle;
+            margin-right: 10px;
+        }
+
+        .footer p {
+            margin: 0;
+            font-size: 14px;
+            color: #ffffff; /* Color del texto en blanco para contraste */
+            display: inline;
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
+        <!-- Contenido de la página -->
+    </div>
+    <div class="footer">
+        <div>
+            <img src="img/logo_ong.jpeg" alt="Logo">
+            <p>Unity class | Khan Academy &#x2714;</p>
+            <img src="img/images.png" alt="Logo"><br>
+            <p>&copy; Copyright 2024</p><br>
+            <p>&#174; Khan Academy</p>
+        </div>
+    </div>
+</body>
+</html>
+
