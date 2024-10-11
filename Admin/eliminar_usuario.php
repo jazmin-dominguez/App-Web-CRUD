cdd43db2a150d4749b016e596e58d7c5bd42ac05
Jazmin Dominguez <147115172+jazmin-dominguez@users.noreply.github.com>
2024-10-10 22:20:23 -0600

    Modifique palabras a ingles y tambien agregue un icono en la tabla eliminar usuario

--- Admin/eliminar_usuario.php
+++ Admin/eliminar_usuario.php
@@ -2,7 +2,7 @@
                     <form method="post" class="space-y-6">
                         <div>
                             <label for="idmodificar" class="block text-sm font-medium text-gray-700">
-                                <i class="fa-solid fa-user mr-2"></i> Seleccionar usuario
+                                <i class="fas fa-user-tag"></i> Select User
                             </label>
                             <select name="idmodificar" id="idmodificar" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                 <?php
@@ -15,7 +15,7 @@
                                             echo "<option value='" . $registro["id"] . "'>" . $registro["nombre"] . "</option>";
                                         }
                                     } else {
-                                        echo "<option>No se encontraron resultados</option>";
+                                        echo "<option>No resoults found</option>";
                                     }
                                 ?>
                             </select>
@@ -23,7 +23,7 @@
             
                         <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
                             <div>
-                                <input type="submit" name="cargar" value="Eliminar Usuario" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium px-4 py-2 bg-red-500 text-white font-bold rounded-md hover:bg-red-600">
+                                <input type="submit" name="cargar" value="Delete User" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium px-4 py-2 bg-red-500 text-white font-bold rounded-md hover:bg-red-600">
                             </div>
                         </div>
                     </form>
@@ -33,6 +33,6 @@
                 if (isset($_POST['cargar'])) {
                     $id = $_POST['idmodificar'];
                     $obj->eliminar_usuario($id); // Asegúrate de tener esta función en tu clase Contacto
-                    echo "<p class='text-blue-600 mt-4'>Usuario eliminado con éxito</p>";
+                    echo "<p class='text-blue-600 mt-4'>User successfully deleted</p>";
                     
                 }
