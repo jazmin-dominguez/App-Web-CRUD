b3ccb090b4cd55a8d9e22f092bc9b23c3a4a9fe1
Jazmin Dominguez <147115172+jazmin-dominguez@users.noreply.github.com>
2024-11-17 22:08:30 -0600

    Se creo la funcion para mostrar graficas de los programas creados

---
+++ Admin/obtener_graficos_programas.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../Conexion/contacto.php';
+
+if (isset($_GET['periodo'])) {
+    $periodo = $_GET['periodo'];
+    $obj = new Contacto();
+    $data = $obj->obtenerRegistrosProgramas($periodo);
+    echo json_encode($data);
+} else {
+    echo json_encode([]);
+}
+?>
